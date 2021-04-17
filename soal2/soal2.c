dd5911b43aa723771057a94b4c825675e998f548
zuhairajamt <80949224+zuhairajamt@users.noreply.github.com>
2021-04-17 21:48:22 +0700

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
 
+int main(){
+    pid_t child_id = fork();
+
+    child_id = fork();
+
+    //extract
+    while(wait(NULL) > 0);
+    if(child_id == 0){
+        char *argv[] = {"unzip", "pets.zip", "-d", "home/traveller/modul2/petshop/", NULL};
+        execv("/usr/bin/unzip", argv)
+    }
+}
