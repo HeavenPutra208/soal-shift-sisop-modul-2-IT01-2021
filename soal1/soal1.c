b67c0eafdb6ee648c01d94c596fbcb8309be29a1
AlessandroTionardo <73150833+AlessandroTionardo@users.noreply.github.com>
2021-04-17 21:46:41 +0700

    Update soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -10,69 +10,80 @@
 #include <time.h>
 #include <wait.h>
 
-int main() {
-  pid_t pid, sid;
 
-  pid = fork();     
+int main(){
 
-  if (pid < 0) {
-    exit(EXIT_FAILURE);
-  }
 
-  if (pid > 0) {
-    exit(EXIT_SUCCESS);
-  }
+    pid_t pid, sid;     
+    pid_t child_id = fork();
+    pid = fork();   
+    int status;
 
-  umask(0);
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
 
-  sid = setsid();
-  if (sid < 0) {
-    exit(EXIT_FAILURE);
-  }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
 
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
+    umask(0);
 
-  while (1) {
-    
-  pid_t child_id;
-  child_id = fork();
-  int status;
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
 
-  if (child_id == 0) {
-    char *argv[] = {"mkdir", "Musyik", "Fylm", "Pyoto",  NULL};
+    // if ((chdir("/")) < 0) {
+    //     exit(EXIT_FAILURE);
+    // }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    while (1) {
+
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+        char now[80];
+        strftime(now, 80, "%Y-%m-%d %H:%M:%S", tm);
+        if(strcmp(now,"2021-04-09 16:22:00") == 0){
+
+if (child_id == 0) {
+	char *argv[] = {"mkdir", "Musyik", "Fylm", "Pyoto",  NULL};
     execv("/bin/mkdir", argv);
-  }
+	}
 
-  child_id = fork();
+	child_id = fork();
     if (child_id == 0) {
-      char *argv[] = {"Wget --no-check-certificate ", "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "-O", "Musik_for_Stevany.zip",  NULL};
+	  while ((wait(&status)) > 0);
+      char *argv[] = {"Wget --no-check-certificate ", "-q","https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "-O", "Musik_for_Stevany.zip",  NULL};
       execv("/usr/bin/wget", argv);
     }
 
 	child_id = fork();
 	if (child_id == 0) {
 	  while ((wait(&status)) > 0);
-      char *argv[] = {"Wget --no-check-certificate ", "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "-O", "Foto_for_Stevany.zip",  NULL};
+      char *argv[] = {"Wget --no-check-certificate ", "-q","https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "-O", "Foto_for_Stevany.zip",  NULL};
       execv("/usr/bin/wget", argv);
     }
 
 	child_id = fork();
 	if (child_id == 0) {
 	  while ((wait(&status)) > 0);
-      char *argv[] = {"Wget --no-check-certificate ", "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "-O", "Film_for_Stevany.zip",  NULL};
+      char *argv[] = {"Wget --no-check-certificate ", "-q","https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "-O", "Film_for_Stevany.zip",  NULL};
       execv("/usr/bin/wget", argv);
     }
   
-  while(wait(NULL) > 0);
+  	while(wait(NULL) > 0);
     child_id = fork();
-    if (child_id == 0) {
-      char *argv[] = {"unzip", "*.zip", NULL};
-      execv("/usr/bin/unzip", argv);
+    	if (child_id == 0) {
+    	char *argv[] = {"unzip", "*.zip", NULL};
+    	execv("/usr/bin/unzip", argv);
     }
 
-  	while(wait(NULL) > 0);
+	while(wait(NULL) > 0);
     child_id = fork();
     	if (child_id == 0) {
     	char *argv[] = {"mv", "MUSIK", "Musyik", NULL};
@@ -91,27 +102,42 @@ int main() {
     	if (child_id == 0) {
     	char *argv[] = {"mv", "FILM", "Fylm", NULL};
     	execv("/bin/mv", argv);
+    }          
+
+            break;
+
+        }
+        
     }
 
-	while(wait(NULL) > 0);
-	child_id = fork();
-    if (child_id == 0) {
-        char *argv[] = {"zip", "-r", " Lopyu_Stevany.zip", "Musyik", "Pyoto", "Fylm", NULL};
-        execv("/usr/bin/zip", argv);
+    while (1) {
 
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+        char now[80];
+        strftime(now, 80, "%Y-%m-%d %H:%M:%S", tm);
+        if(strcmp(now,"2021-04-09 22:22:00") == 0){
+
+          child_id = fork();
+            if (child_id == 0) {
+                char *argv[] = {"zip", "-r", " Lopyu_Stevany.zip", "Musyik", "Pyoto", "Fylm", NULL};
+                execv("/usr/bin/zip", argv);
+
+            }
+
+          while(wait(NULL) > 0);
+            child_id = fork();
+              if (child_id == 0) {
+              char *argv[] = {"rm ", "-r", "Musyik", "Pyoto", "Fylm", NULL};
+              execv("/bin/rm", argv);
+            }
+
+            break;
+
+        }
+    
     }
+  
 
-	while(wait(NULL) > 0);
-    child_id = fork();
-    	if (child_id == 0) {
-    	char *argv[] = {"rm ", "-r", "Musyik", "Pyoto", "Fylm", NULL};
-    	execv("/bin/rm", argv);
-    }
-	
-
-
-
-    sleep(30);
-  }
 }
 
